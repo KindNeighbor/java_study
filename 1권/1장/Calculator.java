6839093de2a4a784dd818eb935a82b186042c193
KindNeighbor <97837003+KindNeighbor@users.noreply.github.com>
2022-04-11 00:32:35 +0900

    Chpater1

---
+++ 1권/1장/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+
+    public int add(int num1, int num2) {
+        int sum;
+        sum = num1 + num2;
+        return sum;
+    }
+
+    public int subtract(int num1, int num2) {
+        int result;
+        result = num1 - num2;
+        return result;
+    }
+
+    public int multiply(int num1, int num2) {
+        int result;
+        result = num1*num2;
+        return result;
+    }
+
+    public int divide(int num1, int num2) {
+        int result;
+        result = num1/num2;
+        return result;
+    }
+}
