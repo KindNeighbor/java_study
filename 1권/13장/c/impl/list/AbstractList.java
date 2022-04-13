f4aeb984cfc6a7a678bbeb2cd07bc0f34742feb1
KindNeighbor <97837003+KindNeighbor@users.noreply.github.com>
2022-04-13 13:21:29 +0900

    Chapter 13

---
+++ 1권/13장/c/impl/list/AbstractList.java
@@ -0,0 +1,7 @@
+package c.impl.list;
+
+import java.util.List;
+
+public abstract class AbstractList implements List {
+    public abstract void clear();
+}
---
+++ 1권/13장/c/impl/list/LIst.java
@@ -0,0 +1,7 @@
+package c.impl.list;
+
+public interface LIst {
+    public void add();
+    public void update(int index, Object value);
+    public void remove(int index);
+}
